9e35d7f6654a1a90bd3b3f008c78faaf979662b0
Aulia Tegar Rahman <cmenk@hacker.ps>
2015-08-05 22:10:10 +0700

    Versi 1.01

---
+++ up.php
@@ -0,0 +1,19 @@
+<html>
+<head>   
+<link href="assets/dropzone.css" type="text/css" rel="stylesheet" />
+<link href="assets/bootstrap.css" type="text/css" rel="stylesheet" />
+<script src="assets/dropzone.js"></script>
+<script src="assets/jquery.js"></script>
+<script src="assets/bootstrap.js"></script>
+
+<Title>Upload Data</Title>
+</head>
+<body >
+    <nav><a></a></nav>
+    <div class="form-group">
+        
+    <form action="upload.php" class="dropzone"></form>
+    </div>
+</body>
+</html>
+ 
